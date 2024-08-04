abc29c7dbe56692fc1b437546dca3559da049038
Chanel101 <vercaemstchanel@gmail.com>
2024-08-04 19:45:45 +0200

    mijn projecten aangepast + javascript

--- js/javascript.js
+++ js/javascript.js
@@ -2,3 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     // Voeg je interactiviteit toe hier
     console.log('Document is geladen!');
 });
+
+
+
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = 'block';
+}
+
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = 'none';
+}
+
+// Sluit de pop-up wanneer de gebruiker buiten de inhoud klikt
+window.onclick = function(event) {
+    if (event.target.classList.contains('popup')) {
+        event.target.style.display = 'none';
+    }
+}

b4792af20de11758b12e4d255a6672c5ed2f7d75
Chanel101 <vercaemstchanel@gmail.com>
2024-08-13 12:41:50 +0200

    chaos voor pop up

--- js/javascript.js
+++ js/javascript.js
@@ -3,15 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Definieer de constante voor de lijst met afbeeldingen
     const imageList = [
-        { src: 'img/1holyraviolieindposter.png', title: 'Holy Ravioli Eindposter', description: 'Bij het vak vormgeving leerde ik werken met kleur en patronen. Daarnaast kreeg ik ook een layering & overprinting als stijl die ik moest toepassen.' },
-        { src: 'img/2chanelcinema.png', title: 'Chanel Cinema', description: 'Beschrijving van project 2: Dit project gaat over een filmfestival branding.' },
-        { src: 'img/3nsticketmachine.png', title: 'NS Ticketmachine', description: 'Beschrijving van project 3: Dit project betreft de herontwerpen van een ticketmachine voor de NS.' },
-        { src: 'img/4kapiteinsnorhaar.png', title: 'Kapitein Snorhaar', description: 'Beschrijving van project 4: Illustratieproject voor een kinderboek.' },
-        { src: 'img/5hemacupcakes.png', title: 'HEMA Cupcakes', description: 'Beschrijving van project 5: Productfotografie voor HEMA cupcakes.' },
-        { src: 'img/6newproductdevelopment.png', title: 'New Product Development', description: 'Beschrijving van project 6: Ontwerp en ontwikkeling van een nieuw product.' },
-        { src: 'img/7arduino.jpg', title: 'Arduino Project', description: 'Beschrijving van project 7: Een hardware project gemaakt met Arduino.' },
-        { src: 'img/8expoprojectincluded.JPG', title: 'Expo Project Included', description: 'Beschrijving van project 8: Deelname aan een lokale expositie.' },
-        { src: 'img/9satisfyers.png', title: 'Satisfyers Project', description: 'Beschrijving van project 9: Een productontwerp voor een klant.' }
+        { src: 'img/1holyraviolieindposter.png', title: 'Holy Ravioli Eindposter', description: 'Bij het vak vormgeving leerde ik werken met kleur en patronen. Daarnaast kreeg ik ook een layering & overprinting als stijl die ik moest toepassen. Ik leerde hier hoe ik moet afstappen van mijn idee en verschillende dingen moet leren uitproberen, wat toch best moeilijk was. Hier wil ik graag verder in groeien om een goede ontwerper te worden.' },
+        { src: 'img/2chanelcinema.png', title: 'Chanel Cinema', description: 'Ik mocht een website ontwerpen in alleen html en css over een onderwerp naar keuze. In de zomervakantie voor school begon was ik al begonnen met de basis te leren over html en css om er zo voor te zorgen dat ik het coderen al herkende en dat het niet volledig nieuw zou zijn voor mij. Het was heel leuk dat ik het mocht doen over wat ik wilde, daarom heb ik gekozen voor films omdat ik heel erg graag over films leer en ze ook graag kijk.' },
+        { src: 'img/3nsticketmachine.png', title: 'NS Ticketmachine', description: 'Tijdens het vak UCD leerde ik werken over hoe je het beste onderzoek doet naar de doelgroep en wat er het beste past voor de doelgroep. Hier heb ik deskresearch gedaan, voorbeelden gezocht in mijn eigen omgeving, een persona gemaakt, interviews gedaan met reizigers op het station, ...' },
+        { src: 'img/4kapiteinsnorhaar.png', title: 'Kapitein Snorhaar', description: 'We moesten aan het einde van Blok 1 een duurzaam project kiezen in Amsterdam en daarvoor een website coderen in html en css, met alleen eigen content. Ik heb beeldmateriaal gebruikt die ik getrokken had van de Poezenboot. Jammer genoeg was het asiel in verbouwing en heb ik geen beeldne kunnen maken van de katten zelf, als alternatief had ik al afbeeldingen van verschillende katten en heb ik die gebruikt. Daarnaast heb ik alles zelf getekend via Illustrator.' },
+        { src: 'img/5hemacupcakes.png', title: 'HEMA Cupcakes', description: 'Bij het vak Human Computer Interaction kreeg ik de opdracht om voor de Hema een gebruiksvriendelijkere interface te ontwerpen. Al vrij snel had ik door dat er heel wat verschillende ontwerpen zijn die je kan maken, maar dat het niet zo makkelijk is. Je moet met best wel wat verschillende aspecten rekening houden.' },
+        { src: 'img/6newproductdevelopment.png', title: 'New Product Development', description: 'New Product Development, daar leerde ik met verschillende ontwerpmethodes te werken om de ervaring voor een bepaalde persona die naar de film gaat bij Pathé te verbeteren. De verschillende ontwerpmethodes die ik hier geleerd heb zijn: Crazy 8, dotvoting, ideekaart, conceptkaart, Harris profiel, ...' },
+        { src: 'img/7arduino.jpg', title: 'Arduino Project', description: 'Emerging technologie was helemaal nieuw voor mij, maar super interessant. Naast het leren werken met AI en Unity, had ik meer motivatie om te leren werken met Arduino en heb ik hiervoor ook een automatisch luikje gemaakt met een motor en een LED-strip die gebruikt zou kunnen worden voor een kattenbak. Ik had graag ook met een speaker leren werken maar dat is me jammer genoeg niet gelukt. Dit had ik heel graag willen laten werken maar na 2 dagen proberen heb ik dat opgegeven.' },
+        { src: 'img/8expoprojectincluded.JPG', title: 'Expo Project Included', description: 'Aah, project Included was een ervaring. Hier kozen we een onderwerp. Guess what? Ik koos het onderwerp "filmnerds" Hiervoor ontworp ik in duo een website waar de community van mijn opleiding samen kan komen en zien wie er in de opleiding een filmliefhebber is. We hadden ook een analoog aspect toegevoegd: The Walk of Filmgekkies, hier kreeg je een walk of fame met sterren waarop een qr-code staat die je kan scannen en hier kan je de filmgekkie van de opleiding beter leren kennen. Op de website kan je ook jezelf toevoegen en verwerken wij de makers van de website, de filmgekkie in hun favoriete filmposter. Dit hadden we gemaakt in Figma.' },
+        { src: 'img/9satisfyers.png', title: 'Satisfyers Project', description: 'Het passieporject was het laatste project van het jaar. Na brainstormen met mijn team van 4, kwamen we tot een consensus om een boek te maken over het plezier van de vrouw/mensen die zich identificeren als vrouw. Ons boek moest ook een maatschappelijke waarde hebben en dat werd voor ons emancipatie. Na drie weken hadden we een boek in elkaar gezet van ongeveer 25 bladzijdes. In deze drie weken heb ik in Illustrator de satisfyers getekend, heb ik de tekst geschreven voor de satisfyers rubriek, het orgasme rubriek en ook de referentie pagina heb ik onderzoek gedaan. Daarnaast hebben we op school een Makerslab, hier heb ik de stickermachine, de lasercutter, de riso-printer, de vacuummachine getest en gekeken wat er voor ons het beste was. We hebben ook interactieve elementen toegepast in ons boek zoals; een spiegeltje, en een vacuumgetrokken vulva in EVA foam. Het boek neemt de lezer mee met ons hoofdpersonage en laat de lezer hun lichaam beter leren kennen en leert hun ook minder schaamte te hebben over hun vulva/vagina/borsten en lichaamshaar.' }
     ];
 
     // Variabele om de huidige index van de afbeelding bij te houden
@@ -26,14 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const modalElement = document.createElement('div');
         modalElement.id = 'imageModal';
         modalElement.className = 'modal';
-        modalElement.innerHTML = `
-            <div class="modal-content">
-                <span class="close">&times;</span>
-                <img class="modal-image" src="${imageInfo.src}" alt="${imageInfo.title}">
-                <h2 class="modal-title">${imageInfo.title}</h2>
-                <p class="modal-description">${imageInfo.description}</p>
-            </div>
-        `;
 
         document.body.appendChild(modalElement);
 
